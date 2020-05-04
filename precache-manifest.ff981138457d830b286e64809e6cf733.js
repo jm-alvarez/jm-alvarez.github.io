0b64b06c02aaa6f6d00bc3a799163e93f8a7f173
José Miguel Álvarez Vañó <jose-miguel.alvarez@gft.com>
2020-05-04 11:25:15 +0200

    Updates

---
+++ precache-manifest.ff981138457d830b286e64809e6cf733.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bccb1a902e64f98a95e3f86e7456772c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c9a2fb70b7566e16560",
+    "url": "/static/css/main.dbc8e06f.chunk.css"
+  },
+  {
+    "revision": "e7f9ed9d9aa5fb8c9d97",
+    "url": "/static/js/2.cc5d45b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cc5d45b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c9a2fb70b7566e16560",
+    "url": "/static/js/main.d5649a88.chunk.js"
+  },
+  {
+    "revision": "04fea4ed8a4524d6c5f6",
+    "url": "/static/js/runtime-main.bd77ae50.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
