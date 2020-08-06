7e5f0fbd69413815d2107567c3df31aa069b2015
José Miguel Álvarez Vañó <jmalvarez@MacBook-Pro-de-Jose.local>
2020-08-06 18:01:31 +0200

    Updates

---
+++ precache-manifest.1fe342a0678484a2c0df4e450428d115.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15071a1a7bad7d06203a4dee441737f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acad98a637b3d1da8378",
+    "url": "/static/css/main.b58b520c.chunk.css"
+  },
+  {
+    "revision": "e7f9ed9d9aa5fb8c9d97",
+    "url": "/static/js/2.cc5d45b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cc5d45b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acad98a637b3d1da8378",
+    "url": "/static/js/main.56d169a9.chunk.js"
+  },
+  {
+    "revision": "04fea4ed8a4524d6c5f6",
+    "url": "/static/js/runtime-main.bd77ae50.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
